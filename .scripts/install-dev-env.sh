934a162b39a1a412600e77de3cbb0d631d4aeef9
sylvain@virtualbox <sylvain.marty@aol.fr>
2017-08-19 12:17:50 +0200

    Mise en place de la nouvelle archi Jekyll

---
+++ .scripts/install-dev-env.sh
@@ -0,0 +1,34 @@
+# From https://gorails.com/setup/ubuntu/16.04
+
+## Load some dependencies
+
+sudo apt-get update
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs
+
+
+## Install ruby (with RVM)
+
+sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 2.4.0
+rvm use 2.4.0 --default
+ruby -v
+
+
+## Install bundler
+
+gem install bundler
+
+
+## Testing ruby version
+
+bundler --version
+
+
+## Installing Jekyll
+
+gem install jekyll
+
+jekyll --version
